deb6c1517d8a7ee206e5888033268d069133ada7
AmitPress <amitkrsurjo@gmail.com>
2022-12-02 22:35:55 +0600

    repetitions accepted

--- Introductory-Problems/Repetitions.py
+++ Introductory-Problems/Repetitions.py
@@ -1,14 +1,20 @@
-sq = [i for i in input().split()] # ACCCGTC
-s = set([sq[0]])
-max_val = 1
-cnt = 0
-for i in range(1, len(sq)):
-    if len(s) == 1:
-        cnt += 1
-        max_val = max(max_val, cnt)
-        s.add(sq[i])
+# I, here, have tried to implement this with a stack
+# the idea is to push consequtive chars in a stack a keep count of it
+
+sq = [i for i in input().split()][0] # ACCCGTC
+s = []
+maxx = 1
+current_letter = sq[0]
+for i in range(0, len(sq)):
+    if sq[i] == current_letter:
+        s.append(sq[i])
+        maxx = max(maxx, len(s))
     else:
-        s.pop(0)
-        cnt = 0
-print(max_val) # wrong ans
+        current_letter = sq[i]
+        s.clear()
+        s.append(sq[i])
+        maxx = max(maxx, len(s))
+    
+
+print(maxx) # wrong ans
     
